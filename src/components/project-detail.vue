953193f5ffcb6e9172aec69fc7b4edcb1f116134
grshaff <gshaffaat@gmail.com>
2025-07-21 21:48:46 +0700

    project detail update

--- src/components/project-detail.vue
+++ src/components/project-detail.vue
@@ -1,14 +1,14 @@
 <template>
     <section class="py-8 bg-white md:py-1 md:mt-8 mx-4 lg:mx-6">
-    <div class="max-w-screen-xl px-4 mx-auto 2xl:px-0">
-      <div class="lg:flex gap-6">
-        <div class="grow sm:max-w-md md:max-w-xl w-full mx-auto">
+    <div class="max-w-screen-xl px-4 2xl:px-0 mx-auto">
+      <div class="lg:flex gap-6 ">
+        <div class="grow sm:max-w-md md:max-w-xl xl:max-w-4xl w-full mx-auto lg:max-w-3xl">
           <!-- Slider -->
             <div data-hs-carousel='{
                 "loadingClasses": "opacity-0"
             }' class="relative">
             <div class="hs-carousel flex flex-col md:flex-row gap-2">
-                <div class=" relative grow overflow-hidden min-h-50 min-w-64 w-full lg:min-h-80 bg-white rounded-lg ">
+                <div class=" relative grow overflow-hidden min-h-50 min-w-64 w-full lg:min-h-80 xl:min-h-100 bg-white rounded-lg ">
                 <div class="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
                     <div class="hs-carousel-slide">
                     <div class="flex justify-center h-full bg-gray-100 p-6">
@@ -45,8 +45,8 @@
                 </button>
                 </div>
 
-                <div class="md:order-1 flex-none ">
-                <div class="hs-carousel-pagination max-h-96 flex flex-row md:flex-col gap-2 justify-center">
+                <div class="md:order-1 flex-none my-auto">
+                <div class="hs-carousel-pagination max-h-96 flex flex-row md:flex-col gap-2 justify-evenly">
                     <div class="hs-carousel-pagination-item shrink-0 border border-gray-200 rounded-md overflow-hidden cursor-pointer size-20 hs-carousel-active:border-blue-400">
                     <div class="flex justify-center items-center text-center size-full bg-gray-100 p-2">
                         <span class="text-xs text-gray-800 transition duration-700">First slide</span>
@@ -70,7 +70,7 @@
 <!-- End Slider -->
         </div>
 
-        <div class="lg:max-w-sm mt-6 sm:mt-8 lg:mt-0">
+        <div class="lg:max-w-sm mt-6 sm:mt-8 lg:mt-0 xl:min-w-sm 2xl:min-w-xl">
           <h1
             class="text-xl font-semibold text-gray-900 sm:text-2xl"
           >
